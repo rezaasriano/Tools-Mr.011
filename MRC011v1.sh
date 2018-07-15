fb24a100651e96856b21876f72de44cc626d3b95
Mr.L00L33 <39614596+rezaasriano@users.noreply.github.com>
2018-07-15 17:44:38 +0700

    Add files via upload

---
+++ MRC011v1.sh
@@ -0,0 +1,387 @@
+clear
+echo "\033[34;1m Selamat Datang di Tool Mr.C011"
+sleep 1
+echo "\033[35;1m Anda disini dapat menginstall berbagai tools" | lolcat
+sleep 1
+echo "\033[36;1m Tetapi tool ini masih beta jadinya masih akan dikembangin lagi" | lolcat
+sleep 1
+echo "\033[31;1m:) Semoga Bermanfaat (:"
+sleep 1
+clear
+echo $red
+cd module
+cat asw.txt
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : Mr.C011"
+echo "\033[35;1mversion Tools: 1"
+echo "\033[35;1mTeam : Mojokerto Noobs Team"
+echo "\033[31;1mPlease For Subcribe Youtube chanel:"
+echo "\033[92mDS TUTORIAL" 
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo "[01] Spam Sms Tokped" | lolcat
+echo "[02] Spam Sms Telkomnyet" | lolcat
+echo "[03] Nyari Cc Buat Carding" | lolcat
+echo "[04] Install meTAInstall" | lolcat
+echo "[05] Litespam untuk spam sms" | lolcat
+echo "[06] Hammer untuk ddos website" | lolcat
+echo "[07] Red_Hawk untuk scan website" | lolcat
+echo "[08] Install WPScan" | lolcat
+echo "[09] install sqlscan" | lolcat
+echo "[10] Install sqlmap" | lolcat
+echo "[11] Install santet-online buat nyantet orang" | lolcat
+echo "[12] Install VbugMaker" | lolcat
+echo "[13] Install Kalinet-hunter" | lolcat
+echo "[14] Install Metasploit" | lolcat
+echo "[15] Install Script deface Creator" | lolcat
+echo "[16] Install XSStrike" | lolcat
+echo "[17] Install D-TECT" | lolcat
+echo "[18] Install Nmap" | lolcat
+echo "[19] Install BlackBox" | lolcat
+echo "[20] Install Infoga" | lolcat
+echo "[21] Install Lazymux" | lolcat
+echo "[22] Install Mbf Hack Akun Fb" | lolcat
+echo "[23] Install V-Create [ Virus Terbaru ]" | lolcat
+echo "[24] Install Webdav Buat Deface" | lolcat
+echo "[25] Install Ktp/KK Generate" | lolcat
+echo "[0] Exit" | lolcat
+
+echo "\033[35;1m Silahkan pilih yang mau anda install"
+
+read -p "#Mr.C011 ~>#" pilihan
+
+if [ $pilihan = "1" ] || [ $pilihan = "1" ]
+then
+clear
+toilet "Mr.C011" 
+php 1.php
+fi
+
+if [ $pilihan = "2" ] || [ $pilihan = "2" ]
+then
+clear
+toilet "Mr.C011"
+pkg install php
+php t.php
+fi
+
+if [ $pilihan = "3" ] || [ $pilihan = "3" ]
+then
+clear
+toilet "Mr.C011"
+php key.php
+fi
+
+if [ $pilihan = "4" ] || [ $pilihan = "4" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+pkg update && upgrade
+pkg install git
+git clone https://github.com/4L13199/meTAInstall
+chmod +x meTAInstall
+mv meTAInstall $HOME
+cd $HOME/meTAInstall
+sh meTAInstall
+fi
+
+if [ $pilihan = "05" ] || [ $pilihan = "05" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update
+apt upgrade
+pkg install toilet
+pkg install php
+pkg install git
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pilihan = "06" ] || [ $pilihan = "6" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+pkg update
+pkg upgrade
+pkg install python
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi 
+
+if [ $pilihan = "07" ] || [ $pilihan = "7" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update && apt upgrade
+apt install git 
+apt install php
+apt install php-curl
+apt install php-xml
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+
+if [ $pilihan = "08" ] || [ $pilihan = "8" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update && apt upgrade
+apt install git
+apt install ruby
+git clone https://github.com/wpscanteam/wpscan/
+cd wpscan
+chmod 777 wpscan.rb
+gem install bundle
+bundle install -j5
+ruby wpscan.rb
+fi
+
+if [ $pilihan = "09" ] || [ $pilihan = "9" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+pkg install php
+pkg install git
+git clone http://www.github.com/Cvar1984/sqlscan.git
+cd sqlscan
+chmod +x sqlscan.php
+php sqlscan.php
+fi
+
+if [ $pilihan = "10" ] || [ $pilihan = "10" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update
+apt upgrade
+apt install python
+apt install python2
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $pilihan = "11" ] || [ $pilihan = "11" ]
+then 
+clear
+toilet -f big -F gay "MR . C011"
+apt update
+apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Gameye98/santet-online
+mv santet-online $HOME
+cd $HOME/santet-online
+python2 santet.py
+fi
+
+if [ $pilihan = "12" ] || [ $pilihan = "12" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update && apt upgrade -y
+apt install wget
+apt install python2
+wget -O vbug.zip https://doc-0s-0o-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/e5a7oc1kmqtjj3q840qjo91eihpa81lk/1518789600000/00678403534077713655/*/0BzXPbcyKYA7rUkxHWXdqeVNvMU0?e=download
+unzip vbug.zip
+cd vbug
+python2 vbug.py
+fi
+
+if [ $pilihan = "13" ] || [ $pilihan = "13" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update
+apt upgrade 
+pkg install git
+git clone https://github.com/Hax4us/Nethunter-In-Termux
+cd Nethunter-In-Termux
+chmod +x kalinethunter
+./kalinethunter
+fi
+
+if [ $pilihan = "14" ] || [ $pilihan = "14" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+pkg update
+pkg upgrade
+pkg install wget
+wget https://Auxilus.github.io/metasploit.sh
+sh metasploit.sh
+cd metasploit-framework
+./msfconsole
+fi
+
+if [ $pilihan = "15" ] || [ $pilihan = "15" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update 
+apt upgrade
+apt install git
+apt install python
+apt install python2
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+python2 create.py
+fi
+
+if [ $pilihan = "16" ] || [ $pilihan = "16" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/XSStrike
+cd XSStrike
+pip2 install -r requirements.txt
+python2 xsstrike
+fi
+
+if [ $pilihan = "17" ] || [ $pilihan = "17" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update && apt upgrade
+pkg install python2 
+pkg install git
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+chmod +x d-tect.py
+python2 d-tect.py
+fi
+
+if [ $pilihan = "18" ] || [ $pilihan = "18" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update
+apt upgrade
+apt install nmap
+fi
+
+if [ $pilihan = "19" ] || [ $pilihan = "19" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update 
+apt upgrade
+pkg install python2 wget
+wget https://raw.githubusercontent.com/jothatron/blackbox/master/blackbox.py
+pip2 install requests pexpect passlib
+python2 blackbox.py
+fi
+
+if [ $pilihan = "20" ] || [ $pilihan = "20" ]
+then
+clear
+toilet -f big -F gay "MR . C011"
+apt update
+apt upgrade
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/m4ll0k/infoga
+cd infoga
+python2 infoga.py
+fi
+
+if [ $pilihan = "21" ] || [ $pilihan = "21" ]
+then
+clear
+toilet -f big -F gay "MR. C011"
+apt update && apt upgrade
+apt install git
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+ls
+python2 lazymux.py
+fi
+
+if [ $pilihan = "22" ] || [ $pilihan = "22" ]
+then
+clear
+toilet -f big -F gay "MR. C011"
+apt update && apt upgrade
+pkg install python2 git
+pip2 install mechanize
+git clone https://github.com/pirmansx/mbf
+ls
+mv mbf $HOME
+cd $HOME/mbf
+python2 MBF.py
+fi
+
+if [ $pilihan = "23" ] || [ $pilihan = "23" ]
+then
+clear
+toilet -f big -F gay "MR. C011"
+cd /storage/emulated/0
+git clone https://github.com/amruhu/V-create
+mv V-create $HOME
+cd $HOME/V-create
+python2 1.py
+fi
+
+if [ $pilihan = "24" ] || [ $pilihan = "24" ]
+then
+clear
+toilet -f big -F gay "MR. C011"
+apt update && upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+apt install openssl curl
+pkg install libcurl
+ln -s /sdcard
+cd sdcard
+mkdir webdav
+cd webdav
+curl -k -O https://pastebin.com/raw/HnVyQPtR
+mv HnVyQPtR webdav.py
+python2 webdav.py
+cd
+cd /sdcard/webdav
+python2 webdav.py
+fi
+
+if [ $pilihan = "25" ] || [ $pilihan = "25" ]
+then
+clear
+toilet -f big -F gay "MR. C011"
+pkg install git
+git clone https://github.com/zerosvn/ktpkkgenerate
+mv ktpkkgenerate $HOME
+cd $HOME/ktpkkgenerate
+php zerosvn.php
+fi
+
+if [ $pilihan = "0" ] || [ $bro = "0" ]
+then
+echo "\033[32;1mMOJOKERTO NOOBS TEAM"
+sleep 1
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo " We Are Legion"
+sleep 1
+echo "We Do Not For Give"
+sleep 1
+echo "We Do Not For Get"
+sleep 1
+echo "EXPECT US"
+sleep 1
+exit
+fi
